d0e0a88846e28ee8668c0ab4bf045540864807c0
Jan Fridrichovsky <jan.fridrichovsky@thermofisher.com>
2020-11-05 20:20:56 +0100

    Access tokens implemented - not working

--- Runtastic2Strava/Form1.cs
+++ Runtastic2Strava/Form1.cs
@@ -15,22 +15,52 @@ using Strava.NET.Model;
 
 namespace Runtastic2Strava
 {
+    public class TokenModel
+    {
+        public string token_type { get; set; }
+        public string access_token { get; set; }
+        public string refresh_token { get; set; }
+        public int expires_at { get; set; }
+    }
+
 	public partial class Form1 : Form
 	{
-		public Form1()
+        static TokenModel token;
+        static TokenModel RenewToken()
+        {
+            Dictionary<string, string> values = new Dictionary<string, string>();
+            values.Add("grant_type", "refresh_token");
+            values.Add("client_id", "55818");
+            values.Add("client_secret", "f7517d6028515a37d54eb5a22f2d1a252d93097c");
+            values.Add("refresh_token", "54993ccfc053f726380c3551cfef430701e06d7c");
+            Configuration.DefaultApiClient.RestClient.BaseUrl = "https://www.strava.com/oauth";
+            RestSharp.RestResponse result = Configuration.DefaultApiClient.CallApi("token", RestSharp.Method.POST, values,
+                "grant_type=refresh_token&client_id=55818&client_secret=f7517d6028515a37d54eb5a22f2d1a252d93097c&refresh_token=ed9d3d6c85b3806a03bfb75e5b9bc41ee6edddfb",
+                values,
+                values, new Dictionary<string, RestSharp.FileParameter>(), new string[0] { }) as RestSharp.RestResponse;
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Configuration.DefaultApiClient.RestClient.BaseUrl = Configuration.DefaultApiClient.BasePath;
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<TokenModel>(result.Content);
+            }
+            return new TokenModel();
+        }
+        public Form1()
 		{
 			InitializeComponent();
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
+            token = RenewToken();
             // Configure OAuth2 access token for authorization: strava_oauth
-            Configuration.Default.AccessToken = "YOUR_ACCESS_TOKEN";
+            Configuration.ApiKey.Add("access_token", "1673f57e5ae557ad251c0e559e4afab3ab106c74");
+            Configuration.ApiKey.Add("refresh_token", "ed9d3d6c85b3806a03bfb75e5b9bc41ee6edddfb");
 
             ActivitiesApi apiInstance = new ActivitiesApi();
             var name = "Running";  // string | The name of the activity.
             var type = "Run";  // string | Type of activity. For example - Run, Ride etc.
-            var startDateLocal = "20201104";  // string | ISO 8601 formatted date time.
+            var startDateLocal = "2020-11-06T00:05:19Z";  // string | ISO 8601 formatted date time.
             var elapsedTime = 56;  // int? | In seconds.
             var description = "Toto je popisek";  // string | Description of the activity. (optional) 
             float distance = 3.4F;  // float? | In meters. (optional)
