c9c82daafcf481b68edc4b18d6f1a3a20d2c33e9
Jan Fridrichovsky <jan.fridrichovsky@thermofisher.com>
2020-11-06 12:46:35 +0100

    Cleanup

--- Runtastic2Strava/MainForm.cs
+++ Runtastic2Strava/MainForm.cs
@@ -15,24 +15,6 @@ using Newtonsoft.Json;
 
 namespace Runtastic2Strava
 {
-	public class EpochDateTimeConverter : Newtonsoft.Json.JsonConverter
-	{
-		public override bool CanConvert(Type objectType)
-		{
-			return objectType == typeof(DateTime);
-		}
-
-		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-		{
-			var t = long.Parse(reader.Value.ToString());
-			return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(t);
-		}
-
-		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-		{
-			throw new NotImplementedException();
-		}
-	}
 	public partial class MainForm : Form
 	{
 		private BindingList<RuntasticActivity> _blRuntasticActivities = new BindingList<RuntasticActivity>();
@@ -42,14 +24,6 @@ namespace Runtastic2Strava
 			InitializeComponent();
 			
 		}
-
-		private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-		public static DateTime FromUnixTime(long unixTime)
-		{
-			return epoch.AddSeconds(unixTime);
-		}
-
 		private void btnLoad_Click(object sender, EventArgs e)
 		{
 			String sPathSportSessions = tbPath.Text + "Sport-sessions";
@@ -65,7 +39,6 @@ namespace Runtastic2Strava
 			}
 			dgvImport.DataSource = _blRuntasticActivities;
 		}
-
 		private void btnBrowse_Click(object sender, EventArgs e)
 		{
 			FolderBrowserDialog fbdExport = new FolderBrowserDialog();
@@ -74,7 +47,6 @@ namespace Runtastic2Strava
 				tbPath.Text = fbdExport.SelectedPath;
 			}
 		}
-
 		private void btnImport_Click(object sender, EventArgs e)
 		{
 			_token = CStravaImporter.RenewToken();
@@ -90,18 +62,23 @@ namespace Runtastic2Strava
 				//DataRow rActivity = _dtRuntasticActivities.Rows[0];
 				//String createdAt = rActivity["created_at"].ToString();
 
+				foreach (RuntasticActivity ac in _blRuntasticActivities)
+				{
+					DetailedActivity result = apiInstance.CreateActivity(
+																			"Running",
+																			"Run",
+																			ac.created_at.ToString("o"),
+																			ac.duration,
+																			"Imported from Runtastic",
+																			ac.distance,
+																			56,
+																			"", // string | List of native photo ids to attach to the activity. (optional) 
+																			56
+																		) ;
+					break;
+				}
 
-				//DetailedActivity result = apiInstance.CreateActivity(
-				//	"Running" + i++ ,
-				//	"Run",
-				//	createdAt ,
-				//	Int32.Parse(rActivity["duration"].ToString()), 
-				//	"Imported from Runtastic",
-				//	Int32.Parse(rActivity["distance"].ToString()),
-				//	56, 
-				//	"", // string | List of native photo ids to attach to the activity. (optional) 
-				//	56);
-				//}
+
 			}
 			catch (Exception except)
 			{
